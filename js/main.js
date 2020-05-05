10aa82735f030ff6436572daff3a1031f23643c0
Xander11rgn <39264955+Xander11rgn@users.noreply.github.com>
2020-05-05 17:51:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,12 +20,11 @@ $(document).ready(function () {
     prevArrow: '<div class="left-control"></div>',
     nextArrow: '<div class="right-control"></div>',
     asNavFor: '.line-control',
-    responsive: [
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1
-        }
+    responsive: [{
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1
+      }
       // },
       // {
       //   breakpoint: 480,
@@ -35,8 +34,7 @@ $(document).ready(function () {
       //     centerPadding: '40px',
       //     slidesToShow: 1
       //   }
-      }
-    ]
+    }]
   });
 
   $('.line-control').slick({ // настройка навигации
@@ -72,7 +70,7 @@ $(document).ready(function () {
 
 const progressbar = document.querySelector('.progressbar');
 window.addEventListener('scroll', function (e) {
-  var s = document.documentElement.scrollTop;
+  var s = document.documentElement.scrollTop || document.body.scrollTop;
   var h = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   progressbar.style.width = s / h * 100 + '%';
 });
@@ -236,15 +234,193 @@ buttons.forEach(el => el.addEventListener('mouseout',
 ));
 
 
-// buttons.forEach(el => el.addEventListener('click',
-//   function () {
-//     anime({
-//       targets: this,
-//       scale: 1,
-//       position: 'top 50% left 50%',
-//       duration: 200,
-//       direction: 'alternate',
-//       easing: 'linear'
-//     });
-//   }
-// ));
+const submitInput = document.querySelector('#submit');
+const emailInput = document.querySelector('#email');
+const nameInput = document.querySelector('#uname');
+const msgInput = document.querySelector('#msg');
+
+//name validation
+nameInput.addEventListener('blur', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  if (uname == "") {
+    anime({
+      targets: '.error1',
+      opacity: 1,
+      duration: 300,
+      easing: 'linear'
+    });
+    submitInput.disabled = true;
+  }
+
+});
+
+nameInput.addEventListener('input', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  anime({
+    targets: '.error1, .error2, .error3',
+    opacity: 0,
+    duration: 300,
+    easing: 'linear'
+  });
+  if (uname == "") {
+    anime({
+      targets: submitInput,
+      scale: 1,
+      boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.59)',
+      duration: 300,
+      easing: 'easeOutBack'
+    });
+    submitInput.disabled = true;
+  };
+  if ((msg != "") && (email != "")) {
+    submitInput.disabled = false;
+
+  };
+});
+
+//email validation
+emailInput.addEventListener('blur', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  if (email == "") {
+    anime({
+      targets: '.error2',
+      opacity: 1,
+      duration: 300,
+      easing: 'linear'
+    });
+    submitInput.disabled = true;
+  };
+});
+
+emailInput.addEventListener('input', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  anime({
+    targets: '.error1, .error2, .error3',
+    opacity: 0,
+    duration: 300,
+    easing: 'linear'
+  });
+  if (email == "") {
+    anime({
+      targets: submitInput,
+      scale: 1,
+      boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.59)',
+      duration: 0,
+      easing: 'easeOutBack'
+    });
+    submitInput.disabled = true;
+  };
+  if ((msg != "") && (uname != "")) {
+    submitInput.disabled = false;
+
+  };
+});
+
+//msg validation
+msgInput.addEventListener('blur', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  if (msg == "") {
+    anime({
+      targets: '.error3',
+      opacity: 1,
+      duration: 300,
+      easing: 'linear'
+    });
+    submitInput.disabled = true;
+  };
+});
+
+msgInput.addEventListener('input', function () {
+  var uname = nameInput.value;
+  var email = emailInput.value;
+  var msg = msgInput.value;
+  anime({
+    targets: '.error1, .error2, .error3',
+    opacity: 0,
+    duration: 300,
+    easing: 'linear'
+  });
+
+  if ((email != "") && (uname != "")) {
+    submitInput.disabled = false;
+
+  };
+});
+
+$(document).ready(function () {
+  $('#submit').click(function () {
+
+    var uname = $('#uname').val();
+    var email = $('#email').val();
+    var msg = $('#msg').val();
+    console.log(uname + email + msg);
+    $.ajax({
+      url: '/php/feedback.php',
+      type: 'POST',
+      cache: false,
+      data: {
+        'uname': uname,
+        'email': email,
+        'msg': msg
+      },
+      dataType: 'text',
+      success: function (data) {
+        if (data == "Success") {
+          popup('Ваша заявка успешно отправлена!');
+        } else {
+          popup('<p style="color:red">Произошла ошибка. Проверьте корректность введенных данных и повторите попытку.</p>');
+        }
+
+      },
+    });
+  });
+});
+
+
+function popup(text) {
+  var prev = document.querySelector('.my-message');
+  try {
+    prev.parentNode.removeChild(prev);
+  } catch (error) {
+
+  }
+  var container = document.createElement('div');
+  container.innerHTML = '<div class="my-message">' + text +
+    '<br><input class="my-message-ok" type="button" value="OK"/> \
+                        </div>';
+  var elem = container.firstChild;
+
+  elem.style.position = 'fixed';
+  // var scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  elem.style.top = 20 + 'px';
+  elem.style.left = Math.floor(document.body.clientWidth / 2) - 150 + 'px';
+
+  var button = elem.querySelector('.my-message-ok');
+  button.addEventListener('click', function () {
+
+    document.querySelectorAll('#submit').forEach(el => el.disabled = true);
+    document.querySelectorAll('#uname, #email, #msg').forEach(el => {
+      el.style.fontStyle = 'normal';
+      el.value = '';
+      el.disabled = false;
+    });
+    elem.style.opacity = '0';
+    elem.style.transition = 'all 0.5s';
+
+  })
+
+  document.querySelectorAll('#uname, #email, #msg, #submit').forEach(el => el.disabled = true);
+  document.querySelectorAll('#uname, #email, #msg').forEach(el => el.style.fontStyle = 'italic');
+  document.body.appendChild(elem);
+  
+}
